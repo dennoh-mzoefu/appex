4440690ce5334a57b30df32aa50c33846c0a7f22
Denis Gichana <96231487+dennoh-mzoefu@users.noreply.github.com>
2022-04-07 14:57:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,35 @@ fclose($log);
 
 $callbackContent = json_decode($stkCallbackResponse);
 
-$ResultCode = $callbackContent["Body"]["stkCallback"]["ResultCode"];
+// API URL
+$url = 'http://appextrading.unaux.com/appex/pages/mpesa/callbackurl.php';
+
+// Create a new cURL resource
+$ch = curl_init($url);
+
+// Setup request to send json via POST
+$data = array(
+    'username' => 'codexworld',
+    'password' => '123456'
+);
+$payload = json_encode(array("user" => $data));
+
+// Attach encoded JSON string to the POST fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+// Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+// Return response instead of outputting
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the POST request
+$result = curl_exec($ch);
+
+// Close cURL resource
+curl_close($ch);
 echo "hey";
-echo $ResultCode;
+
 // $CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
 // $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
 // $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value;
