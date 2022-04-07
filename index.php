d544c85f86c893fc801ed4b6ae749cee97d13e73
Denis Gichana <96231487+dennoh-mzoefu@users.noreply.github.com>
2022-04-07 21:11:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,22 @@
 <?php
-header("Content-Type: application/json");
+ 		header("Content-Type: application/json");
 
-$stkCallbackResponse = file_get_contents('php://input');
-$logFile = "stkTinypesaResponse.json";
-$log = fopen($logFile, "a");
-fwrite($log, $stkCallbackResponse);
-fclose($log);
-
-$callbackContent = json_decode($stkCallbackResponse);
-
-$ResultCode = $callbackContent->Body->stkCallback->ResultCode;
-$CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
-$Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
-$MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value;
-$PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->Value;
-
-if ($ResultCode == 0) {
-    $servername = "sql206.unaux.com";
-    $username = "unaux_31448661";
-    $password = "kl3aiyxrtlo";
-    $dbname = "unaux_31448661_trial";
-
-    // Create connection
-//     $conn = new mysqli($servername, $username, $password, $dbname);
-    $conn = mysqli_connect('sql206.unaux.com', 'unaux_31448661', 'kl3aiyxrtlo', 'unaux_31448661_trial');
+     $response = '{
+         "ResultCode": 0, 
+         "ResultDesc": "Confirmation Received Successfully"
+     }';
  
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $insert = $conn->query("INSERT INTO tinypesa(CheckoutRequestID,ResultCode,amount,MpesaReceiptNumber,PhoneNumber) VALUES ('$CheckoutRequestID','$ResultCode','$Amount','$MpesaReceiptNumber','$PhoneNumber')");
-
-    $conn = null;
+     // DATA
+     $mpesaResponse = file_get_contents('php://input');
+ 
+     // log the response
+     $logFile = "M_PESAConfirmationResponse.txt";
+ 
+     // write to file
+     $log = fopen($logFile, "a");
+ 
+     fwrite($log, $mpesaResponse);
+     fclose($log);
+ 
+     echo $response;
 }
