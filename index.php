8f6b619a3db6296ae24b270cc821f920245d494b
Denis Gichana <96231487+dennoh-mzoefu@users.noreply.github.com>
2022-04-07 23:12:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
      $mpesaResponse = file_get_contents('php://input');
  
      // log the response
-     $logFile = "M_PESAConfirmationResponse.txt";
+     $logFile = "http://appextrading.unaux.com/daraja/M_PESAConfirmationResponse.txt";
  
      // write to file
      $log = fopen($logFile, "a");
